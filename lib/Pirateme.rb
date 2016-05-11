51e005278b1cfa1f867122bbe3cb120ec3eceeff
Aaron1515 <aaron1515@gmail.com>
2016-05-11 13:39:43 -0700

    add attacks

--- lib/Pirateme.rb
+++ lib/Pirateme.rb
@@ -12,7 +12,15 @@ module Pirateme
 
   module Fake
     def self.captian
-      ["Captian Morgan", "Portgas D. Ace", "Captain Jessamiah", "Morgan Adams", "Captian America", "Captian Henry Avery", "Tom Ayrton", "Captain Barrett"].sample
+      "Captian " + ["Morgan", "Portgas D. Ace", "Jessamiah", "Morgan Adams", "America", "Henry Avery", "Tom Ayrton", "Barrett", "Edward Teach - Blackbeard", "White Beard", "Black Beard", "Drake", "Ralegh", "Roberts", "Cook", "Nelson", "Silver", "Kirk", "Magellen", "Columbus", ].sample
+    end
+
+    def self.attack
+      ["stab", "jab", "kick"].sample
+    end
+
+    def self.phrase
+
     end
 
 
