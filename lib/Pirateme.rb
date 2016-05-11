a216bece1bcd9899694f00abb81ca739b5d510fa
Aaron1515 <aaron1515@gmail.com>
2016-05-11 13:55:34 -0700

    add jokes

--- lib/Pirateme.rb
+++ lib/Pirateme.rb
@@ -23,6 +23,29 @@ module Pirateme
       ["Walk the plank.", "Aaaarrrrgggghhhh!", "Ahoy, Matey.", "Ahoy, me Hearties!", "Dead men tell no tales.", "Feed the fish.", "Hang 'im from the yardarm", "Son of a Biscuit Eater", "Thar she blows!", "Weigh anchor and hoist the mizzen!", "Yo Ho Ho" ].sample
     end
 
+    def self.joke
+      [
+        "What does a pirate think happens at the end of time? Arrrrmageddon.",
+        "What's a pirate's favorite food? Arrrrrtichokes.",
+        "What's a pirate's favorite basketball move? Jump hook.",
+        "How do pirates make their money? By hook or by crook.",
+        "Why do pirates make excellent fishermen? They know how to hook the big ones.",
+        "Where do pirates find their birds? Parrots Without Partners.",
+        "Did you hear about the pirate's parrot that fell in love with a duck? The bird kept saying, \"Polly wants a quacker\".",
+        "Why couldn't the young pirate see the R-rated movie? There was no parrot-al guidance.",
+        "Why should pirates work for FedEx? They have the fastest ships in the shipping business.",
+        "What's it called when a pirate's sloop runs aground? It's ship out of luck.",
+        "Why don't pirates use a safe deposit box? They put their valuables in Davy Jones' Locker.",
+        "Why did the pirate refuse to say, \"Aye, Aye, Captain\"? Because he's only got one eye.",
+        "What's the pirate's favorite restaurant? Trick question because it's either Jolly Roger or Long John Silver's.",
+        "How could the pirate acquire the ship so cheaply? Because it was on sail.",
+        "Why did the pirate not learn how to bowl? He had a severe hook.",
+        "What are the only notes a pirate can sing? High C's [seas]",
+        "What shivers at the bottom of the sea? a nervous wreck",
+        "Why don't pirates take a bath before they walk the plank? They'll just wash up on shore later",
+        "What's the smelliest part of a pirate ship? the poop deck",
+        "What's the worse nightmare for a pirate on a blind date? a sunken chest with no booty"
+        ].sample
 
   end
 end
