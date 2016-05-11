cd7c5b80a08a3ae539323b2ca67b083872c40a5c
Aaron1515 <aaron1515@gmail.com>
2016-05-11 12:56:37 -0700

    add captian names

--- lib/Pirateme.rb
+++ lib/Pirateme.rb
@@ -4,4 +4,8 @@ module Pirateme
   def self.hello
     "Welcome to the coolest pirate gem!"
   end
+
+  def self.captian
+    ["Captian Morgan", "Portgas D. Ace", "Captain Jessamiah", "Morgan Adams", "Captian America", "Captian Henry Avery", "Tom Ayrton", "Captain Barrett"].sample
+  end
 end
